9871f2d5a30f0b78213e0252848fb324c289f182
wy <wangyan9507@163.com>
2019-10-22 16:26:35 +0800

    THE first STEP OF EP4

--- web/roadm-gui/projects/roadm-gui-lib/src/lib/hero.service.ts
+++ web/roadm-gui/projects/roadm-gui-lib/src/lib/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +11,7 @@ export class HeroService {
   
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
